0a90ac86192c0bf2c3330d76ca0450cf71a44f95
mdemauro <mdemauro@7b138824-e75d-0410-b8ca-a6fb1b7eb368>
2009-01-04 19:33:09 +0000

    XNA - Phong model fully implemented with multiple light support (needs to be refactored for better structure)

--- RayTracerXNA/RayTracerXNA/PhongModel.cs
+++ RayTracerXNA/RayTracerXNA/PhongModel.cs
@@ -13,7 +13,14 @@ namespace RayTracerXNA
         {
             get { return lights; }
             set { lights = value; }
-        }        
+        }
+
+        protected Vector3 cameraPos;
+        public Vector3 CameraPosition
+        {
+            get { return cameraPos; }
+            set { cameraPos = value; }
+        }     
 
         protected Vector4 ambientLight;
         public Vector4 AmbientLight
@@ -30,41 +37,6 @@ namespace RayTracerXNA
         }
         
         #region ILightingModel Members
-
-        public Color CalculateLighting(Primitive p, Vector3 intersection)
-        {
-            Vector4 diffuseTotal = Vector4.Zero;
-            Vector4 specularTotal = Vector4.Zero;
-            foreach (Light light in lights)
-            {
-                diffuseTotal += calculateDiffuse(p, intersection, light);
-                specularTotal += calculateSpecular();
-            }
-
-            Vector4 colorVector = calculateAmbient(p) + 
-                Vector4.Multiply(diffuseTotal, p.DiffuseStrength) +
-                Vector4.Multiply(specularTotal, p.SpecularStrength);
-
-            return new Color(colorVector);
-        }
-
-        private Vector4 calculateAmbient(Primitive p)
-        {
-            return p.AmbientStrength * p.MaterialColor * ambientLight;
-        }
-
-        private Vector4 calculateDiffuse(Primitive p, Vector3 intersection, Light l)
-        {
-            Vector4 diffuseColor = l.LightColor * p.MaterialColor;
-            Vector3 lightVector = Vector3.Normalize(intersection - l.Position);
-            float diffuseAmount = Vector3.Dot(lightVector, p.GetIntersectNormal(intersection));
-            return Vector4.Multiply(diffuseColor, diffuseAmount);
-        }
-
-        private Vector4 calculateSpecular()
-        {
-            return Vector4.Zero;
-        }
 
         #endregion
     }
